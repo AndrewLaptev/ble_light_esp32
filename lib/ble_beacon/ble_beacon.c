290146a66009abf5a655fbbf447ba008315523d9
Andrew Laptev <nickname.avast@gmail.com>
2022-05-19 12:26:22 +0300

    fix: delete old ibeacon esp_gap_cb Events of iBeacon add in gap_event_handler() (common.c)

--- lib/ble_beacon/ble_beacon.c
+++ lib/ble_beacon/ble_beacon.c
@@ -12,9 +12,6 @@
 
 extern esp_ble_ibeacon_vendor_t vendor_config;
 
-///Declare static functions
-static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
-
 #if (IBEACON_MODE == IBEACON_RECEIVER)
 static esp_ble_scan_params_t ble_scan_params = {
     .scan_type              = BLE_SCAN_TYPE_ACTIVE,
@@ -35,86 +32,6 @@ static esp_ble_adv_params_t ble_adv_params = {
     .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
 };
 #endif
-
-
-static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) {
-    esp_err_t err;
-
-    switch (event) {
-    case ESP_GAP_BLE_ADV_DATA_RAW_SET_COMPLETE_EVT:{
-#if (IBEACON_MODE == IBEACON_SENDER)
-        esp_ble_gap_start_advertising(&ble_adv_params);
-#endif
-        break;
-    }
-    case ESP_GAP_BLE_SCAN_PARAM_SET_COMPLETE_EVT: {
-#if (IBEACON_MODE == IBEACON_RECEIVER)
-        //the unit of the duration is second, 0 means scan permanently
-        uint32_t duration = 0;
-        esp_ble_gap_start_scanning(duration);
-#endif
-        break;
-    }
-    case ESP_GAP_BLE_SCAN_START_COMPLETE_EVT:
-        //scan start complete event to indicate scan start successfully or failed
-        if ((err = param->scan_start_cmpl.status) != ESP_BT_STATUS_SUCCESS) {
-            ESP_LOGE(BEACON_TAG, "Scan start failed: %s", esp_err_to_name(err));
-        }
-        break;
-    case ESP_GAP_BLE_ADV_START_COMPLETE_EVT:
-        //adv start complete event to indicate adv start successfully or failed
-        if ((err = param->adv_start_cmpl.status) != ESP_BT_STATUS_SUCCESS) {
-            ESP_LOGE(BEACON_TAG, "Adv start failed: %s", esp_err_to_name(err));
-        }
-        break;
-    case ESP_GAP_BLE_SCAN_RESULT_EVT: {
-        esp_ble_gap_cb_param_t *scan_result = (esp_ble_gap_cb_param_t *)param;
-        switch (scan_result->scan_rst.search_evt) {
-        case ESP_GAP_SEARCH_INQ_RES_EVT:
-            /* Search for BLE iBeacon Packet */
-            if (esp_ble_is_ibeacon_packet(scan_result->scan_rst.ble_adv, scan_result->scan_rst.adv_data_len)){
-                esp_ble_ibeacon_t *ibeacon_data = (esp_ble_ibeacon_t*)(scan_result->scan_rst.ble_adv);
-                ESP_LOGI(BEACON_TAG, "----------iBeacon Found----------");
-                esp_log_buffer_hex("IBEACON_DEMO: Device address:", scan_result->scan_rst.bda, ESP_BD_ADDR_LEN );
-                esp_log_buffer_hex("IBEACON_DEMO: Proximity UUID:", ibeacon_data->ibeacon_vendor.proximity_uuid, ESP_UUID_LEN_128);
-
-                uint16_t major = ENDIAN_CHANGE_U16(ibeacon_data->ibeacon_vendor.major);
-                uint16_t minor = ENDIAN_CHANGE_U16(ibeacon_data->ibeacon_vendor.minor);
-                ESP_LOGI(BEACON_TAG, "Major: 0x%04x (%d)", major, major);
-                ESP_LOGI(BEACON_TAG, "Minor: 0x%04x (%d)", minor, minor);
-                ESP_LOGI(BEACON_TAG, "Measured power (RSSI at a 1m distance):%d dbm", ibeacon_data->ibeacon_vendor.measured_power);
-                ESP_LOGI(BEACON_TAG, "RSSI of packet:%d dbm", scan_result->scan_rst.rssi);
-            }
-            break;
-        default:
-            break;
-        }
-        break;
-    }
-
-    case ESP_GAP_BLE_SCAN_STOP_COMPLETE_EVT:
-        if ((err = param->scan_stop_cmpl.status) != ESP_BT_STATUS_SUCCESS){
-            ESP_LOGE(BEACON_TAG, "Scan stop failed: %s", esp_err_to_name(err));
-        }
-        else {
-            ESP_LOGI(BEACON_TAG, "Stop scan successfully");
-        }
-        break;
-
-    case ESP_GAP_BLE_ADV_STOP_COMPLETE_EVT:
-        if ((err = param->adv_stop_cmpl.status) != ESP_BT_STATUS_SUCCESS){
-            ESP_LOGE(BEACON_TAG, "Adv stop failed: %s", esp_err_to_name(err));
-        }
-        else {
-            ESP_LOGI(BEACON_TAG, "Stop adv successfully");
-        }
-        break;
-
-    default:
-        break;
-    }
-}
-
 
 esp_err_t ble_ibeacon_app_register(void) {
     esp_err_t err;
