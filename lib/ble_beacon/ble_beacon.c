1f95694df24b8bfba4d464889e494009efffc2bf
Andrew Laptev <nickname.avast@gmail.com>
2022-05-19 12:33:30 +0300

    fix: delete ble_adv_params from ble_beacon.c Instead adv_params in common.c

--- lib/ble_beacon/ble_beacon.c
+++ lib/ble_beacon/ble_beacon.c
@@ -21,16 +21,6 @@ static esp_ble_scan_params_t ble_scan_params = {
     .scan_window            = 0x30,
     .scan_duplicate         = BLE_SCAN_DUPLICATE_DISABLE
 };
-
-#elif (IBEACON_MODE == IBEACON_SENDER)
-static esp_ble_adv_params_t ble_adv_params = {
-    .adv_int_min        = 0x20,
-    .adv_int_max        = 0x40,
-    .adv_type           = ADV_TYPE_NONCONN_IND,
-    .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
-    .channel_map        = ADV_CHNL_ALL,
-    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
-};
 #endif
 
 esp_err_t ble_ibeacon_app_register(void) {
